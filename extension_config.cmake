8b1d5ab68ca0800106f4cff3940d793697098323
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-06-02 19:30:18 +0200

    Move to extension-ci-tools workflows

---
+++ extension_config.cmake
@@ -0,0 +1,8 @@
+# Extension from this repo
+duckdb_extension_load(spatial
+    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
+    LOAD_TESTS
+    TEST_PATH test
+    INCLUDE_PATH spatial/include
+    LINKED_LIBS "../../deps/local/lib/lib*.a"
+)
