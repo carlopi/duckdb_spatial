66a3077bee1f8f87d378379cad9f0e556dca9a2e
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-06-02 20:56:45 +0200

    Fixup includes

--- extension_config.cmake
+++ extension_config.cmake
@@ -2,7 +2,7 @@
 duckdb_extension_load(spatial
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
     LOAD_TESTS
-    TEST_PATH test
-    INCLUDE_PATH spatial/include
+    TEST_PATH test/sql
+    INCLUDE_DIR spatial/include
     LINKED_LIBS "../../deps/local/lib/lib*.a"
 )
